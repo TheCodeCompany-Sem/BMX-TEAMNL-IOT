ed4c24edaca272e39eaf929342c50718726636ec
Mortada M'Rabet <mortada.mrabet@hva.nl>
2022-04-25 11:54:41 +0200

    intialized backend bmx-app

---
+++ bmx-app-backend/bmx-app-backend/src/main/java/com/example/bmxappbackend/BmxAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmxappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmxAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmxAppBackendApplication.class, args);
+	}
+
+}
---
+++ bmx-app-backend/bmx-app-backend/src/test/java/com/example/bmxappbackend/BmxAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmxappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmxAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
