f3bbcceaf34e37000a57e4488b03cce16e6559bd
Martin <47635335+Martiiiiing@users.noreply.github.com>
2022-05-11 13:22:21 +0200

    connected the wemos to the wifi

--- Humidity_sensor_demo/Humidity_sensor_demo.ino
+++ Humidity_sensor_demo/Humidity_sensor_demo.ino
@@ -1,9 +1,39 @@
 #include "DHTesp.h"
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
 DHTesp dht;
+const char* ssid = "Ziggo_ittdesk";
+const char* password = "dekey2017";
+
+void setup_wifi() {
+  // Connect WiFi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname("Name");
+  WiFi.begin(ssid, password);
  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("-");
+    Serial.flush();
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Print the IP address
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
     Serial.begin(115200);
+    setup_wifi();
     dht.setup(D2, DHTesp::DHT22);
+    
 }
  
 void loop() {
