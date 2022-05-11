29fa6111ac721a2bfada4aa5484efaf5e782c9ef
Z. Mssaadi <zakaria.mssaadi@hva.nl>
2022-05-11 10:00:05 +0200

    commit for the splash screen set up again

--- bmx-app/src/app/app-routing.module.ts
+++ bmx-app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './page/homepage/homepage.component';
+const routes: Routes = [
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- bmx-app/src/app/app.module.ts
+++ bmx-app/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
+import {SplashScreenStateService} from "./services/splash-screen.service";
+import { SplashComponent } from './components/splash/splash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [  SplashScreenStateService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
