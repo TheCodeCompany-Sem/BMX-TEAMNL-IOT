49991f8c983135e0e040c6d878f353e50f84dff6
Martin <47635335+Martiiiiing@users.noreply.github.com>
2022-05-18 13:43:53 +0200

    Sensors now work all together

--- Arduino-main/Arduino-main.ino
+++ Arduino-main/Arduino-main.ino
@@ -3,43 +3,9 @@
 DHTesp dht;
 const int RecordTime = 3; //Define Measuring Time (Seconds)
 const int SensorPin = 3;  //Define Interrupt Pin (2 or 3 @ Arduino Uno)
-
+const int DHTPin = 7;    //Define DHT Pin
 int InterruptCounter;
 float WindSpeed;
-
-void setup() {
-    Serial.begin(9600);
-    dht.setup(7);
-}
-
-void loop() {
-    float h = dht.getHumidity();
-    float t = dht.getTemperature();
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print("% - ");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.println("*C");
-    measure();
-    Serial.print("Wind Speed: ");
-    Serial.print(WindSpeed);       //Speed in km/h
-    Serial.print(" km/h - ");
-    Serial.print(WindSpeed / 3.6); //Speed in m/s
-    Serial.println(" m/s");
-    // read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    float voltage = sensorValue*5/1023.0;
-    int direction = map(sensorValue, 0, 1023, 0, 360);
-    // print out the value you read:
-    Serial.print("ADC : ");
-    Serial.println(sensorValue);
-    Serial.print("Voltage : ");
-    Serial.println(voltage);
-    Serial.print("Direction : ");
-    Serial.println(direction);
-    delay(1000);
-}
 
 void measure() {
   InterruptCounter = 0;
@@ -51,4 +17,50 @@ void measure() {
 
 void countup() {
   InterruptCounter++;
+}
+
+void wind_direction(){
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+    float voltage = sensorValue*5/1023.0;
+    int direction = map(sensorValue, 0, 1023, 0, 360);
+    // print out the value you read:
+    Serial.print("ADC : ");
+    Serial.println(sensorValue);
+    Serial.print("Voltage : ");
+    Serial.println(voltage);
+    Serial.print("Direction : ");
+    Serial.println(direction);
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    dht.setup(DHTPin, DHTesp::DHT22);
+}
+
+void loop() {
+    wind_direction();
+    delay(10);
+    
+    float h = dht.getHumidity();
+    float t = dht.getTemperature();
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print("% - ");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println("*C");
+    delay(10);
+
+
+    measure();
+    Serial.print("Wind Speed: ");
+    Serial.print(WindSpeed);       //Speed in km/h
+    Serial.print(" km/h - ");
+    Serial.print(WindSpeed / 3.6); //Speed in m/s
+    Serial.println(" m/s");
+    delay(10);
+    
+    delay(3000);
 }
